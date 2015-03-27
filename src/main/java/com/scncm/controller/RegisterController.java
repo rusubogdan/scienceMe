4763365266fed0cdb4a720e7c5d5492e5ffe3604
pxmihai <mihai.paun@my.fmi.unibuc.ro>
2015-03-27 19:51:49 +0200

    -added register

--- src/main/java/com/scncm/controller/RegisterController.java
+++ src/main/java/com/scncm/controller/RegisterController.java
@@ -1,34 +1,37 @@
 package com.scncm.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-
+@Controller
+@RequestMapping(value = "/register")
 public class RegisterController {
 
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ModelAndView registerFormAfterRequest(){
 
-    @RequestMapping(value = "/register", method = RequestMethod.GET)
-
-    public ModelAndView registerFormAfterRequest(
-
-
-            @RequestParam(value = "email", required = false) String email,
-            @RequestParam(value = "firstName", required = false) String firstName,
-            @RequestParam(value = "firstName", required = false) String lastName,
-            @RequestParam(value = "password", required = false) String password,
-            @RequestParam(value = "gender", required = false) String gender) {
-
-        //
         ModelAndView mv = new ModelAndView("registerForm");
-
-        if (email == null) {
-            mv.addObject("missing", "Please enter your email.");
-        }
 
         return mv;
     }
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ModelAndView submit(
 
+            @RequestParam(value = "email", required = true) String email,
+            @RequestParam(value = "firstName", required = true) String firstName,
+            @RequestParam(value = "lastName", required = true) String lastName,
+            @RequestParam(value = "password", required = true) String password,
+            @RequestParam(value = "gender", required = false) String gender) {
+
+        ModelAndView mv = new ModelAndView("loginForm");
+        if (email == null) {
+            mv.addObject("missing", "Please enter your email.");
+        }
+        return new ModelAndView("submit");
+    }
 
 }
