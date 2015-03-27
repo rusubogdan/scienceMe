d7161f59e90f51b7199dc6cfee4f1d1740d42149
pxmihai <mihai.paun@my.fmi.unibuc.ro>
2015-03-27 16:37:23 +0200

    -attempt at adding RegisterForm Controller and view

--- src/main/java/com/scncm/controller/RegisterController.java
+++ src/main/java/com/scncm/controller/RegisterController.java
@@ -5,18 +5,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Created by mihai7 on 3/27/2015.
- */
+
 public class RegisterController {
 
 
     @RequestMapping(value = "/register", method= RequestMethod.GET)
 
-    public ModelAndView registerForm(
+    public ModelAndView registerFormAfterRequest(
 
 
-            @RequestParam(value = "error",  required = false) String error,
+            @RequestParam(value = "email",  required = true) String email,
+            @RequestParam(value = "firstName",  required = true) String firstName,
+            @RequestParam(value = "firstName",  required = true) String firstName
+            @RequestParam(value = "password",  required = true) String password,
+            @RequestParam(value = "gender",  required = true) String firstName)
+
+
 
 
     )
