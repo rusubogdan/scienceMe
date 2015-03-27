fd2640856ceaeb456fbc8d0fff8249d359d1f2b2
raducc <radu_cancel@yahoo.com>
2015-03-27 22:36:35 +0200

    partea de front end pentru wall

--- src/main/webapp/resources/js/wall.js
+++ src/main/webapp/resources/js/wall.js
@@ -5,21 +5,60 @@ $(document).ready(function () {
         slidesToShow: 1,
         slidesToScroll: 1,
         dots: true,
-        //adaptiveHeight: true,
-//        autoplay: true,
+        autoplay: true,
         autoplaySpeed: 7000,
-        arrows: false,
+        arrows: false
     });
+    wall.init.timeBar();
+    wall.init.buttonSideBar();
 });
 
 var wall = {
-    init: function() {
+    init: {
+        timeBar: function () {
+            $('.range-slider').jRange({
+                from: 0,
+                to: 60,
+                step: 1,
+                scale: [0, 15, 30, 45, 60],
+                format: '%s',
+                width: 150,
+                theme: "theme-blue",
+                showLabels: true,
+                isRange: true
+            });
+        },
+        buttonSideBar: function () {
+            $('#news-button-sidebar').on({
+                click: function () {
+                    if($('#news-button-sidebar').css('background-color') == "rgb(0, 128, 0)"){
+                        $('.sidebar-button').css('background-color','transparent');
+                    }
+                    else{
+                        $('.sidebar-button').css('background-color','transparent');
+                        $('#news-button-sidebar').css('background-color','green');
+                    }
+                }
+            });
+            $('#rating-button-sidebar').on({
+                click: function () {
+                    if($('#rating-button-sidebar').css('background-color') == 'rgb(0, 128, 0)'){
+                        $('.sidebar-button').css('background-color','transparent');
+                    }
+                    else{
+                        $('.sidebar-button').css('background-color','transparent');
+                        $('#rating-button-sidebar').css('background-color','green');
+                    }
+                }
+            });
+
+        }
 
     },
     test: function () {
         $('#test1Button').on({
             click: function () {
-                $.get('wall/ajax/testArticle',  function (response) {
+                $.get('wall/ajax/testArticle', function (response) {
                     console.log(response);
                 });
             }
