635f262afca39f737bcadc73607dd1491f869c79
pxmihai <mihai.paun@my.fmi.unibuc.ro>
2015-03-27 16:44:58 +0200

    -yet another commit, small error resolved.

--- src/main/java/com/scncm/controller/RegisterController.java
+++ src/main/java/com/scncm/controller/RegisterController.java
@@ -9,25 +9,26 @@ import org.springframework.web.servlet.ModelAndView;
 public class RegisterController {
 
 
-    @RequestMapping(value = "/register", method= RequestMethod.GET)
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
 
     public ModelAndView registerFormAfterRequest(
 
 
-            @RequestParam(value = "email",  required = true) String email,
-            @RequestParam(value = "firstName",  required = true) String firstName,
-            @RequestParam(value = "firstName",  required = true) String firstName
-            @RequestParam(value = "password",  required = true) String password,
-            @RequestParam(value = "gender",  required = true) String firstName)
+            @RequestParam(value = "email", required = true) String email,
+            @RequestParam(value = "firstName", required = true) String firstName,
+            @RequestParam(value = "firstName", required = true) String lastName,
+            @RequestParam(value = "password", required = true) String password,
+            @RequestParam(value = "gender", required = true) String gender) {
 
+        // i don't know what i`m doing
+        ModelAndView mv = new ModelAndView("registerForm");
 
+        if (email == null) {
+            mv.addObject("missing", "Please enter your email.");
+        }
 
-
-    )
-
-
-
-
+        return mv;
+    }
 
 
 }
