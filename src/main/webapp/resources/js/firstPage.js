965519774239223d6447a744ec241cb29fd98ca4
pxmihai <mihai.paun@my.fmi.unibuc.ro>
2015-04-01 10:27:40 +0300

    -jquery refactor

--- src/main/webapp/resources/js/firstPage.js
+++ src/main/webapp/resources/js/firstPage.js
@@ -1,13 +1,25 @@
-function show(){
+$(document).ready(function(){
+    $("registerMe").click(function(){
+        $("loginform").style.display="none";
+        $("registerform").style.display="block";
+    })
+    $("logmeIn").click(function(){
+        $("loginform").style.display="block";
+        $("registerform").style.display="none";
+    })
+});
 
+
+/*
+function show(){ //Reg
     document.getElementById("loginform").style.display="none";
     document.getElementById("registerform").style.display="block";
-
 }
 
-function hide(){
-
+function hide(){ //LogIn
     document.getElementById("loginform").style.display="block";
     document.getElementById("registerform").style.display="none";
-
 }
+*/
+
+
