2e68f318c11397572a62b423915acf93a7072ccd
PCosmin <cosmin.posteuca@yahoo.com>
2015-04-04 20:32:17 +0300

    Add Recommendation Engine

--- src/main/java/com/scncm/controller/WallController.java
+++ src/main/java/com/scncm/controller/WallController.java
@@ -6,6 +6,16 @@ import com.scncm.service.ArticleService;
 import com.scncm.service.TagService;
 import com.scncm.service.UserService;
 
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -83,10 +93,20 @@ public class WallController {
 
     @RequestMapping(value = "ajax/testArticle", method = RequestMethod.GET)
     @ResponseBody
-    public Map testArticle() {
+    public Map testArticle() throws TasteException {
         Map map = new HashMap();
 
         map.put("message", "message");
+
+
+        DataModel model = new FileDataModel(new File("/path/to/dataset.csv"));
+        UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+        UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
+        UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+        List<RecommendedItem> recommendations = recommender.recommend(2, 3);
+        for (RecommendedItem recommendation : recommendations) {
+            System.out.println(recommendation);
+        }
 
         return map;
     }
